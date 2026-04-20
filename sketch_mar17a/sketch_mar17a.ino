ac46d3036c310ea64cebde537b454e1da78a3485
Andy <andy@lancito01.dev>
2026-04-20 17:04:15 -0300

    working: simple reaction game, 2 buttons 2 leds

--- sketch_mar17a/sketch_mar17a.ino
+++ sketch_mar17a/sketch_mar17a.ino
@@ -6,11 +6,25 @@ const int mainLedPin = 23;
 const int blueLedPin = 25;
 const int redLedPin = 26;
 
-int buttonPins[] = {mainBtnPin, blueBtnPin, redBtnPin};
-int ledPins[] = {mainLedPin, blueLedPin, redLedPin};
-
 enum State { WAITING_FOR_GAME, PLAYING, GAME_END };
 State currentState = WAITING_FOR_GAME;
+
+int buttonPins[] = {mainBtnPin, blueBtnPin, redBtnPin};
+int ledPins[] = {mainLedPin, blueLedPin, redLedPin};
+int randomWaitTime = random(3000, 6001);
+int msSinceStartOfGame = 0;
+int lastWinner = -1; //* 0 for blue, 1 for red
+bool isMainLedPowered = false;
+
+bool readButton(int buttonPin) {
+  return (digitalRead(buttonPin) == LOW); //* Button pressed: current LOW
+}
+
+void writeLed(int ledPin, bool value) {
+  digitalWrite(ledPin, value ? HIGH : LOW);
+}
+
+bool lastPressed = readButton(mainBtnPin);
 
 void setup() {
   for (int i = 0; i < 3; i++) {
@@ -21,22 +35,52 @@ void setup() {
   Serial.println("> Waiting for user to start game...");
 }
 
-bool readButton(int buttonPin) {
-  return (digitalRead(buttonPin) == LOW); //* Button pressed: current LOW
-}
-
-void writeLed(int ledPin, bool value) {
-  digitalWrite(ledPin, value ? HIGH : LOW);
-}
-
 void loop() {
+  bool currPressed = readButton(mainBtnPin);
+  Serial.println(currentState);
+
   switch (currentState) {
   case WAITING_FOR_GAME:
-    for (int i = 0; i < 3; i++) {
-      writeLed(ledPins[i], readButton(buttonPins[i]));
+    if (currPressed && !lastPressed) { //? user just pressed the button
+      lastPressed = currPressed;
+      writeLed(mainLedPin, isMainLedPowered);
+      writeLed(blueLedPin, false);
+      writeLed(redLedPin, false);
+
+    } else if (!currPressed && lastPressed) { //? user just let go of the button
+      currentState = PLAYING;
+      lastPressed = currPressed;
+      writeLed(mainLedPin, true);
+      delay(randomWaitTime);
+      writeLed(mainLedPin, false);
+      msSinceStartOfGame = millis();
     }
     break;
-  case PLAYING:
+  case PLAYING: {
+    bool bluePlayer = readButton(blueBtnPin);
+    bool redPlayer = readButton(redBtnPin);
+
+    //? blink main led after each iteration
+    writeLed(mainLedPin, isMainLedPowered);
+    isMainLedPowered = !isMainLedPowered;
+
+    if (!bluePlayer && !redPlayer) //* early return
+      break;
+
+    if (bluePlayer) {
+      lastWinner = 0;
+    } else {
+      lastWinner = 1;
+    }
+    currentState = GAME_END;
     break;
+  }
+  case GAME_END:
+    isMainLedPowered = false;
+    writeLed(mainLedPin, isMainLedPowered);
+    writeLed(lastWinner ? redLedPin : blueLedPin, true);
+    break;
+    if (currPressed)
+      currentState = WAITING_FOR_GAME;
   }
 }
